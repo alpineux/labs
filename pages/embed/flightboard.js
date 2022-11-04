326b7daa31a84b6ec7a152d23f908b0e6b54f386
alpineux <99145815+alpineux@users.noreply.github.com>
2022-11-03 22:56:02 -0600

    Update flightboard.js

--- pages/embed/flightboard.js
+++ pages/embed/flightboard.js
@@ -4,13 +4,14 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../../styles/Home.module.css'
 
-import { Table, Badge, Avatar } from 'evergreen-ui'
+import { Table, Badge, Avatar, SideSheet, Spinner, Pane } from 'evergreen-ui'
 
 
 export default function FlightBoard() {
 
 	const [flights, setFlights] = useState({})
 	const [flightsOrig, setFlightsOrig] = useState({})
+	const [isShown, setIsShown] = useState(false)
 
 	const [loading, setLoading] = useState(true)
 
@@ -53,79 +54,78 @@ export default function FlightBoard() {
 
 
 	return(
-	    <body style={{ height: '100%', width: '100%', margin: 0 }}>
-	    	<div style={{ width: '100vw', height: '100%', minHeight: '100vh', boxSizing: 'border-box', overflowX: 'hidden', overflowY: 'hidden' }}>
-		    	{!loading ?
-	            	<div>
-	            		<Table>
-						  <Table.Head>
-						    <Table.SearchHeaderCell 
-						    	placeholder={`Search Flight Number`}
-						    	onChange={(e) => filterTable(e)}
-						    />
-						    <Table.TextHeaderCell>Status</Table.TextHeaderCell>
-						    <Table.TextHeaderCell>Terminal</Table.TextHeaderCell>
-						  </Table.Head>
-						  <Table.Body>
+	    <Pane 
+	    	border="default"
+	    	is="section"
+	    >
+    		<SideSheet isShown={isShown} onCloseComplete={() => setIsShown(false)}>hi</SideSheet>
+    		
+	    	{!loading ?
+            	<div>
+            		<Table>
+					  <Table.Head>
+					    <Table.SearchHeaderCell 
+					    	placeholder={`Search Flight Number`}
+					    	onChange={(e) => filterTable(e)}
+					    />
+					    <Table.TextHeaderCell>Status</Table.TextHeaderCell>
+					    <Table.TextHeaderCell>Terminal</Table.TextHeaderCell>
+					  </Table.Head>
+					  <Table.Body>
 
-	            		{
-	                		flights.departures.map((flight, index) => {
+            		{
+                		flights.departures.map((flight, index) => {
 
-	                			let logo = flight.number.match(/^(\S+)\s(.*)/).slice(1)
-	                			console.log('logo', logo)
+                			let logo = flight.number.match(/^(\S+)\s(.*)/).slice(1)
+                			console.log('logo', logo)
 
-	                			https://content.airhex.com/content/logos/airlines_WG_100_100_s.png
+                			https://content.airhex.com/content/logos/airlines_WG_100_100_s.png
 
-	                			return(
-	                				<Table.Row key={flight.number} isSelectable onSelect={() => alert(flight.name)}>
-								        <Table.TextCell>
-								        	<Avatar
-											  src={`https://content.airhex.com/content/logos/airlines_${logo[0]}_100_100_s.png?background=fff&md5apikey=4d5669b5107fdc240dba0f03961c48e4`}
-											  name="WestJet"
-											  size={20}
-											  marginRight={8}
-											/>
-								        	<b>{flight.number}</b> - {flight.movement.airport.name}
-								        </Table.TextCell>
-								        
-								        <Table.TextCell>
-								        	{flight.status == 'CanceledUncertain' && <Badge color="red">Canceled</Badge> }
-								        	{flight.status == 'Canceled' && 
-								        		<Badge color="red">Canceled</Badge>
-								        	}
+                			return(
+                				<Table.Row key={flight.number} isSelectable onSelect={() => setIsShown(true)}>
+							        <Table.TextCell>
+							        	<Avatar
+										  src={`https://content.airhex.com/content/logos/airlines_${logo[0]}_100_100_s.png?background=fff&md5apikey=4d5669b5107fdc240dba0f03961c48e4`}
+										  name="WestJet"
+										  size={20}
+										  marginRight={8}
+										/>
+							        	<b>{flight.number}</b> - {flight.movement.airport.name}
+							        </Table.TextCell>
+							        
+							        <Table.TextCell>
+							        	{flight.status == 'CanceledUncertain' && <Badge color="red">Canceled</Badge> }
+							        	{flight.status == 'Canceled' && 
+							        		<Badge color="red">Canceled</Badge>
+							        	}
 
-								        	{flight.status == 'Expected' && <Badge color="green">On-Time</Badge> }
-								        	{flight.status == 'Delayed' && <Badge color="yellow">Delayed</Badge> }
-								        	{flight.status == 'Unknown' && <Badge color="red">N/A</Badge> }
-								        	{flight.status == 'Departed' && <Badge color="blue">Departed</Badge> }
-								        	{flight.status == 'GateClosed' && <Badge color="teal">Gate Closed</Badge> }
-								        	{flight.status == 'Boarding' && <Badge color="purple">Boarding</Badge> }	
-								        	
+							        	{flight.status == 'Expected' && <Badge color="green">On-Time</Badge> }
+							        	{flight.status == 'Delayed' && <Badge color="yellow">Delayed</Badge> }
+							        	{flight.status == 'Unknown' && <Badge color="red">N/A</Badge> }
+							        	{flight.status == 'Departed' && <Badge color="blue">Departed</Badge> }
+							        	{flight.status == 'GateClosed' && <Badge color="teal">Gate Closed</Badge> }
+							        	{flight.status == 'Boarding' && <Badge color="purple">Boarding</Badge> }	
+							        	
 
-								        </Table.TextCell>
-								        <Table.TextCell>
-								        	{flight.movement.terminal ?
-								        		<>Terminal {flight.movement.terminal}</>
-								        	:
-								        		<>N/A</>
-								        	}
-								        </Table.TextCell>
-							      	</Table.Row>
-	                			)
-	                		})
-	            		}
-	            		</Table.Body>
-	            		</Table>
-	            	</div>
-	            	
-	            :
-	            	<>Loading...</>
-	        	}
-
-        	</div>
-
-        	<>Powered by AlpineUX</>
-
-	    </body>
+							        </Table.TextCell>
+							        <Table.TextCell>
+							        	{flight.movement.terminal ?
+							        		<>Terminal {flight.movement.terminal}</>
+							        	:
+							        		<>N/A</>
+							        	}
+							        </Table.TextCell>
+						      	</Table.Row>
+                			)
+                		})
+            		}
+            		</Table.Body>
+            		</Table>
+            	</div>
+            	
+            :
+            	<Spinner />
+        	}
+	    </Pane>
 	)
 }
